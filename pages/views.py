b9e0f25c432eebc12eb56a38c95453b29f69648d
SpOOk <spook.devv@gmail.com>
2022-06-10 12:51:03 -0400

    updated html views

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
+    model = "User"
     template_name = "pages/home.html"
+    fields = ['name', 'email']
