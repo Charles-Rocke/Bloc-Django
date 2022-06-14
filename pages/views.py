661defc0430b2136fffc8a99baf8f156888f866a
SpOOk <spook.devv@gmail.com>
2022-06-14 01:13:15 -0400

    logo and modal sign up not complete

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from webauthn import registration
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from accounts.models import CustomUser
+# from webauthn import registration
+from accounts.forms import CustomUserCreationForm
 
 # Create your views here.
-class HomePageView(TemplateView):
-    model = "User"
+class HomePageView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("home")
     template_name = "pages/home.html"
-    
-
-    
-    
