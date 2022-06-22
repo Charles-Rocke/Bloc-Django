c2a49f0be66a00968435fe7ef2df5f22cadf930e
SpOOk <spook.devv@gmail.com>
2022-06-22 14:09:54 -0400

    deployment ready2

--- bloc_project/settings.py
+++ bloc_project/settings.py
@@ -95,8 +95,10 @@ WSGI_APPLICATION = "bloc_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # do I need to change the user and password below???
+DATABASES = {
+    "default": env.dj_db_url("DATABASE_URL", default="postgres://postgres@db/postgres")
+}
 
-DATABASES['default'] = env.dj_db_url.config(default='DATABASE_URL')
 
 
 # Password validation
