ec6ce2d6de59b2d4102b01f1cc91ddcd6e7206c8
SpOOk <spook.devv@gmail.com>
2022-06-22 14:40:22 -0400

    edited static settings

--- bloc_project/settings.py
+++ bloc_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
 from environs import Env
-
+import os
 env = Env()
 env.read_env()
 
@@ -28,7 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["bloc-lp.herokuapp.com", "localhost", "127.0.0.1", "bloc.id"]
+ALLOWED_HOSTS = ["bloc-lp.herokuapp.com", "localhost", "127.0.0.1", "bloc.id", "*"]
 
 
 # Application definition
@@ -134,10 +134,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
